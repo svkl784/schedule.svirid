807f9a0f5f1b95b156c1be3823967d6ad1f28e12
Nataliya Svirid <klybiksvirid@gmail.com>
2021-03-10 22:51:14 +0300

    commit

--- src/main/java/domain/serialization/SerializationTask.java
+++ src/main/java/domain/serialization/SerializationTask.java
@@ -34,11 +34,8 @@ public class SerializationTask {
 
         public static void SerializationObject(List<Task> taskList, String fileName) {
 
-            try {
-                FileOutputStream fos = new FileOutputStream(fileName);
-                ObjectOutputStream oos = new ObjectOutputStream(fos);
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
                 oos.writeObject(taskList);
-                oos.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -46,15 +43,13 @@ public class SerializationTask {
 
         public static void DeserializationObject(String fileName) throws ClassNotFoundException {
 
-            try {
-                FileInputStream fis = new FileInputStream(fileName);
-                ObjectInputStream ois = new ObjectInputStream(fis);
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));) {
                 List<Task> taskList = (List<Task>) ois.readObject();
                 System.out.println("---------------Your saved files----------------------");
                 System.out.println(taskList.toString()
-                                           .replace("[","")
-                                           .replace("]", "")
-                                           .replace(",","\n"));
+                        .replace("[", "")
+                        .replace("]", "")
+                        .replace(",", "\n"));
                 System.out.println("-----------------------------------------------------");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
