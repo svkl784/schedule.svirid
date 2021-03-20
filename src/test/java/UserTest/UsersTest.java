f8b7e7c634cd53edeed04fbd4691072cf18b1d63
Nataliya Svirid <klybiksvirid@gmail.com>
2021-03-20 16:50:51 +0300

    commit

---
+++ src/test/java/UserTest/UsersTest.java
@@ -0,0 +1,47 @@
+package UserTest;
+import domain.users.User;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+import static org.junit.Assume.assumeNotNull;
+import static org.junit.Assume.assumeThat;
+
+@RunWith(Theories.class)
+public class UsersTest {
+    private static User user1;
+    private static User user2;
+
+
+    @BeforeClass
+    public static void createUsers() {
+        user1 = new User("Nataliya", "svk", 56646, "56452ghh54");
+        user2 = new User("Alexander", "kim", 46843, "54746jm458");
+    }
+
+    @DataPoints
+    public static String[] usersName() {
+        return new String[]{ user1.getName(), user2.getName()};
+    }
+
+    @Theory
+    public void testUsersName (String name) throws Exception {
+        Assert.assertEquals("Nataliya", user1.getName());
+//        System.out.println(String.format("Testing with %s and %s", name));
+//        assumeNotNull(name);
+//        assumeThat(name, notNullValue());
+//        String actual= User.createUsers(name);
+//        System.out.println(String.format("Actual: %s \n", actual));
+//        assertThat(actual, is(allOf(containsString(name)));
+    }
+}
